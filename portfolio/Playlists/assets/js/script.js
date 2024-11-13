5546840d35689a4a8822257061005c069988e330
EnzoGusmao <159530740+EnzoGusmao@users.noreply.github.com>
2024-11-13 22:02:10 +0000

    Add files via upload

---
+++ portfolio/Playlists/assets/js/script.js
@@ -0,0 +1,5 @@
+// Função para alternar a visibilidade da barra lateral
+function toggleSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('open');
+}
